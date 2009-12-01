b1e60e2f7f1660f67c9ede764d8d4013926f970f
jen <jen@jens-macbook.local>
2009-12-01 09:44:05 -0500

    removed console msgs

--- static/main.js
+++ static/main.js
@@ -6,25 +6,19 @@ var delayedTagCall = null;
 function getTag() {
 	try {
 		if(selected_tag != true) {
-			console.log("getTag body is running");
 			tag_counter++;
 			$.getJSON('/tag_stream/' + parseInt(tag_counter), function(data){
 				$('ul#tag_stream').prepend($(data));
 				setTimeout(getTag, 1500);
 			});
-		} else {
-			console.log("getTag body saw a flag it didn't like");
-		}
+		} 
 	} catch(err) { }
 }
 
 $(function() {
 	getTag();
-
 	$("ul#tag_stream li a").live("mouseover",function(){
-		console.log("hover in");
 		if(delayedTagCall!=null){
-			console.log("cancelling delayedTagCall..");
 			clearInterval(delayedTagCall);
 			delayedTagCall = null;
 		}
@@ -32,7 +26,6 @@ $(function() {
 	});
 
 	$("ul#tag_stream li a").live("mouseout",function(){
-		console.log("hover out");
 		selected_tag = false;
 		delayedTagCall = setTimeout(getTag,1000);
 	});
