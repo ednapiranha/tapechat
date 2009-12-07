a4c49f8744d3891e1dcd9d157a4c3e265cda0f79
jen <jen@jens-macbook.local>
2009-12-07 10:39:20 -0500

    fixed js highlighting

--- static/static.js
+++ static/static.js
@@ -1,7 +1,7 @@
 $(function() {
 	if($('h1 span').html().length > 1) {
 		var word = $('h1 span').html().replace(/((\/)|(\[)|(\])|(\\)|(\?)|(\.)|(\:)|(;)|(,)|(\!)|(\*)|(\+)|(\=)|({)|(})|(\f)|(\r)|(\n)|(\t)){1,}/i, '');
-		var re = new RegExp("^" + word, "i")
+		var re = new RegExp("^" + word + "$", "i")
 		$('ul.messages li a').each(function() {
 			if($(this).html().match(re)) {
 				$(this).addClass('highlight');
